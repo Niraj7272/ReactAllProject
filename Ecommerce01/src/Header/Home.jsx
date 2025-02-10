16083de559f3869c83df287cadee4cf30d020ff5
Niraj7272 <nc72723@gmail.com>
2025-02-10 21:44:59 +0545

    some changes in home page

--- Ecommerce01/src/Header/Home.jsx
+++ Ecommerce01/src/Header/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='relative'>
+      <img src="src/assets/menfashion.jpg" alt="" className='h-full w-full'/>
+      <div className='bg-gray-500 text-[1.5rem] text-white absolute top-[22.6rem] left-[46rem] w-[13rem] h-[3rem] pl-[3rem] font-bold'>
+        <NavLink to="/shop">Shop Now</NavLink>
+      </div>
+      <div className='absolute top-0 right-0'>
+        <img src="src/assets/images.jpg" alt="" className='h-[40rem]' />
+      </div>
+      <div className='bg-white absolute top-[40rem] left-[61.5rem]'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio reiciendis, inventore eligendi illo, voluptate atque impedit quod quae saepe numquam corrupti? Odit quae mollitia maiores harum atque nostrum at accusamus.</p>
+      </div>
+    </div>
   )
 }
 
--- Ecommerce01/src/NavBar.jsx
+++ Ecommerce01/src/NavBar.jsx
@@ -4,11 +4,17 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='bg-yellow-400 flex justify-between h-[4rem]'>
+      <div>
+        <img src="src/assets/foreveryoung.jpg" alt="" className='h-[3rem] ml-[1rem] mt-[0.5rem]' />
+      </div>
         <div className='flex gap-[2rem] ml-[2rem] mt-[1rem] font-bold text-[1.2rem]'>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/product">product</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/contact">Contact</NavLink>
+        </div>
+        <div>
+          <input type="text" className='bg-white h-[2.4rem] w-[20rem] mt-[0.8rem] rounded-2xl' />
         </div>
         <div className='mr-[2rem] mt-[1rem] font-bold text-[1.2rem]'>
             <NavLink to="/login">Login</NavLink>
