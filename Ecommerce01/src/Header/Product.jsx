ad7874b7cb6fdb44edeef41d4e5b19f3ea4a1d2c
Niraj7272 <nc72723@gmail.com>
2025-02-13 14:24:16 +0545

    design of product page

--- Ecommerce01/src/Header/Product.jsx
+++ Ecommerce01/src/Header/Product.jsx
@@ -3,20 +3,50 @@ import React from 'react'
 const Product = () => {
   return (
       <div className='bg-blue-300 h-auto m-0 flex'>
-        <div className='bg-red-400 h-[20rem] w-[15rem] m-[1rem] mt-[2rem] flex'>
+        <div className='bg-white h-[25rem] w-[15rem] m-[1rem] mt-[2rem] flex relative'>
             <img src="src/assets/tshirtblack.jpg" alt="" className='h-[15rem]' />
+            <div className='absolute top-[16rem] font-bold'>
+                <h1 className='ml-[5rem]'>T-shirt</h1>
+                <p className='ml-[5rem]'>RS.1000</p>
+                <button className='bg-green-700 m-[0.5rem] h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Detail</button><br />
+                <button className='bg-red-700 h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Add Card</button>
+            </div>
         </div>
-        <div className='bg-red-400 h-[20rem] w-[15rem] m-[1rem] mt-[2rem] '>
+        <div className='bg-white h-[25rem] w-[15rem] m-[1rem] mt-[2rem] flex relative'>
             <img src="src/assets/tshirtnew.jpg" alt="" className='h-[15rem]' />
+            <div className='absolute top-[16rem] font-bold'>
+                <h1 className='ml-[5rem]'>T-shirt</h1>
+                <p className='ml-[5rem]'>RS.1000</p>
+                <button className='bg-green-700 m-[0.5rem] h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Detail</button><br />
+                <button className='bg-red-700 h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Add Card</button>
+            </div>
         </div>
-        <div className='bg-red-400 h-[20rem] w-[15rem] m-[1rem] mt-[2rem] '>
+        <div className='bg-white h-[25rem] w-[15rem] m-[1rem] mt-[2rem] flex relative'>
             <img src="src/assets/images.jpeg" alt="" className='h-[15rem]' />
+            <div className='absolute top-[16rem] font-bold'>
+                <h1 className='ml-[5rem]'>T-shirt</h1>
+                <p className='ml-[5rem]'>RS.1000</p>
+                <button className='bg-green-700 m-[0.5rem] h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Detail</button><br />
+                <button className='bg-red-700 h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Add Card</button>
+            </div>
         </div>
-        <div className='bg-red-400 h-[20rem] w-[15rem] m-[1rem] mt-[2rem] '>
+        <div className='bg-white h-[25rem] w-[15rem] m-[1rem] mt-[2rem] flex relative'>
             <img src="src/assets/tshirtblack.jpg" alt="" className='h-[15rem]' />
+            <div className='absolute top-[16rem] font-bold'>
+                <h1 className='ml-[5rem]'>T-shirt</h1>
+                <p className='ml-[5rem]'>RS.1000</p>
+                <button className='bg-green-700 m-[0.5rem] h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Detail</button><br />
+                <button className='bg-red-700 h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Add Card</button>
+            </div>
         </div>
-        <div className='bg-red-400 h-[20rem] w-[15rem] m-[1rem] mt-[2rem] '>
+        <div className='bg-white h-[25rem] w-[15rem] m-[1rem] mt-[2rem] flex relative'>
             <img src="src/assets/tshirtnew.jpg" alt="" className='h-[15rem]' />
+            <div className='absolute top-[16rem] font-bold'>
+                <h1 className='ml-[5rem]'>T-shirt</h1>
+                <p className='ml-[5rem]'>RS.1000</p>
+                <button className='bg-green-700 m-[0.5rem] h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Detail</button><br />
+                <button className='bg-red-700 h-[2rem] w-[11rem] ml-[2rem] text-white cursor-pointer'>Add Card</button>
+            </div>
         </div>
       </div>
   )
