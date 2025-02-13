377c27c750a4f4560452c6fbfcf194ab90273444
Niraj7272 <nc72723@gmail.com>
2025-02-13 15:26:34 +0545

    simple changes in css

--- Daraz_clone/src/NavBar.jsx
+++ Daraz_clone/src/NavBar.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return (
-    <div>
-        <img src="https://lzd-img-global.slatic.net/us/domino/3b870cb043c7f8a9741cbf66329e294e.png" alt="" />
-        <div>
+    <div className='bg-orange-600'>
+        <div className='text-white'>
             <NavLink to="/saveMoreOnApp">SAVE MORE ON APP</NavLink>
             <NavLink to="/becomeASeller">BECOME A SELLER</NavLink>
             <NavLink to="/helpAndSupport">HELP & SUPPORT</NavLink>
@@ -12,6 +11,8 @@ const NavBar = () => {
             <NavLink to="/signUp">SIGN UP</NavLink>
             <NavLink >भाषा परिवर्तन</NavLink>
         </div>
+        <img src="https://lzd-img-global.slatic.net/us/domino/3b870cb043c7f8a9741cbf66329e294e.png" alt="" className='h-[3rem]' />
+
     </div>
   )
 }
