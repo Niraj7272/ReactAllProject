1a21eaaff2e700ed85f364c1e8436ab2dab96779
Niraj7272 <nc72723@gmail.com>
2025-02-13 22:48:55 +0545

    home page and navabar made

--- Daraz_clone/src/NavBar.jsx
+++ Daraz_clone/src/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return (
-    <div className='bg-orange-600'>
-        <div className='text-white'>
+    <div className='bg-orange-600 h-[8rem]'>
+        <div className='text-white text-[0.8rem] ml-[40rem] pt-[0.4rem] flex gap-[2rem]'>
             <NavLink to="/saveMoreOnApp">SAVE MORE ON APP</NavLink>
             <NavLink to="/becomeASeller">BECOME A SELLER</NavLink>
             <NavLink to="/helpAndSupport">HELP & SUPPORT</NavLink>
@@ -11,8 +11,10 @@ const NavBar = () => {
             <NavLink to="/signUp">SIGN UP</NavLink>
             <NavLink >भाषा परिवर्तन</NavLink>
         </div>
-        <img src="https://lzd-img-global.slatic.net/us/domino/3b870cb043c7f8a9741cbf66329e294e.png" alt="" className='h-[3rem]' />
-
+        <div className='flex'>
+            <img src="https://lzd-img-global.slatic.net/us/domino/3b870cb043c7f8a9741cbf66329e294e.png" alt="" className='h-[3rem] ml-[5rem] mt-[1rem]' />
+            <input type="text" className='bg-white h-[2.5rem] w-[40rem] ml-[5rem] mt-[1rem]' />
+        </div>
     </div>
   )
 }
--- Daraz_clone/src/Pages/Home.jsx
+++ Daraz_clone/src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <img src="src/assets/70d96363-eff1-41d6-beb4-0d24afc1ba70_NP-1976-688.png_2200x2200q80.png_.webp" alt="" className='h-[22rem] ml-[4rem]' />
+    </div>
   )
 }
 
