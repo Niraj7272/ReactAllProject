e41763aa5fee329c313e8d60bbd28fc9b199d5bf
Niraj7272 <nc72723@gmail.com>
2025-02-17 14:29:02 +0545

    simple

--- Daraz_clone/src/Category/Samba.jsx
+++ Daraz_clone/src/Category/Samba.jsx
@@ -18,15 +18,16 @@ const Samba = () => {
         <NavLink to="#">TV,Audio / Video Gaming</NavLink>{">"}
         <NavLink to="#">TV,Audio / Video Gaming</NavLink>
       </div>
-      <div className='flex justify-between bg-white ml-[4rem] mr-[4rem]'>
-        <div className='ml-[2rem] pl-[2rem] pb-[3rem]'>
-          <img src="src/assets/7ba5486ce9bde718f24d3738b64c677a.jpg_400x400q80.jpg_.webp" alt="" />
+      <div className='flex ml-[4rem] mr-[4rem] w-[78rem]'>
+        <div className=' pl-[2rem] pb-[3rem] w-[25rem] bg-white'>
+          <img src="src/assets/7ba5486ce9bde718f24d3738b64c677a.jpg_400x400q80.jpg_.webp" alt="" className='h-[15rem]'/>
         </div>
-        <div className=''>
-          <h1>Airpods Bluetooth Charging Case Pure Sound Super Premium Quality</h1>
+        <div className='w-[30rem] bg-white'>
+          <h1 className='text-[1.4rem] mt-[1rem]'>Airpods Bluetooth Charging Case Pure Sound Super Premium Quality</h1>
         </div>
-        <div className='bg-gray-50'>
-          <h1>Delivery Options </h1>
+        <div className='bg-gray-50 w-[21rem]'>
+          <h1 className='mt-[1rem] ml-[1rem] text-gray-600'>Delivery Options </h1>
+          <h1 className='ml-[3rem] mr-[5rem]'>Bagmati, Kathmandu Metro 22 - Newroad Area, Newroad</h1>
         </div>
       </div>
     </div>
