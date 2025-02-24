adb971c6bf758e343d1398373ecc2a79f6b1beb4
Niraj7272 <nc72723@gmail.com>
2025-02-24 15:37:19 +0545

    little changes

--- Todo_list/src/AddList.jsx
+++ Todo_list/src/AddList.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { Mycontext } from "./ToDoList";
 
-const AddList = ({cross}) => {
+const AddList = () => {
+  const { setPop } = useContext(Mycontext);
 
   return (
     <div className="bg-blue-100 h-[25rem] w-[27rem] ml-[30rem] mt-[8rem]">
@@ -8,26 +10,31 @@ const AddList = ({cross}) => {
         <h1 className="font-bold text-[1.7rem] pl-[10rem] pt-[1.7rem]">
           Add List
         </h1>
-        <h1 className="bg-white h-[1.5rem] w-[1.5rem] pl-[0.3rem] text-[1rem] text-red-600 font-bold cursor-pointer hover:text-[1.2rem]" onClick={()=>{
-          cross(false)
-        }}>X</h1>
+        <h1
+          className="bg-white h-[1.5rem] w-[1.5rem] pl-[0.3rem] text-[1rem] text-red-600 font-bold cursor-pointer hover:text-[1.2rem]"
+          onClick={() => {
+            setPop(false);
+          }}
+        >
+          X
+        </h1>
       </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Title"
-            className="border-[1px] m-[1rem] w-[23rem] h-[2rem] pl-[10rem] ml-[2rem]"
-          />
-          <br />
-          <textarea
-            name=""
-            id=""
-            cols="48"
-            rows="5"
-            placeholder="Description"
-            className="border-[1px] ml-[2rem]"
-          ></textarea>
-        </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Title"
+          className="border-[1px] m-[1rem] w-[23rem] h-[2rem] pl-[10rem] ml-[2rem]"
+        />
+        <br />
+        <textarea
+          name=""
+          id=""
+          cols="48"
+          rows="5"
+          placeholder="Description"
+          className="border-[1px] ml-[2rem]"
+        ></textarea>
+      </div>
       <div>
         <button className="bg-green-400 ml-[2rem] w-[23rem] mt-[2rem] text-[1.5rem] cursor-pointer">
           Submit
--- Todo_list/src/ToDoList.jsx
+++ Todo_list/src/ToDoList.jsx
@@ -1,62 +1,66 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
 import AddList from "./AddList";
 
-const TODoList = ()=>{
+export const Mycontext = createContext();
+const TODoList = () => {
+  const [pop, setPop] = useState(false);
+  const [data, setdata] = useState([
+    {
+      title: "hello",
+      description: "hello everyone how are you. i am fine and how aru",
+    },
+  ]);
 
-    const [pop,setPop] = useState(false);
-
-    const data = [
-        {
-            title:"hello",
-            description:"hello everyone how are you. i am fine and how aru"
-        },
-        {
-            title:"hello",
-            description:"hello everyone how are you. i am fine and how aru"
-        },
-       
-    ]
-
-
-    return(
-        <div className="bg-gray-200 h-screen">
-            <h1>.</h1>
-            <div>
-                <h1 className="bg-white m-[1rem] rounded-[20px] h-[5rem] text-[3rem] font-bold pl-[33rem]">Todo List</h1>
-            </div>
-            <div className="flex justify-between">
-                <div className="bg-white flex justify-between w-[30rem] ml-[1.5rem] border-[1px]">
-                    <input type="text" className="w-[30rem]"/>
-                    <img src="https://img.icons8.com/?size=100&id=59878&format=png&color=000000" alt="" className="h-[2rem] bg-orange-300 p-1 cursor-pointer" />
-                </div>
-                <div className="bg-green-600 mr-[10rem] w-[12rem] pl-[5rem] pt-[0.3rem] font-bold text-white cursor-pointer hover:bg-green-800">
-                    <button onClick={()=>{
-                        setPop(true);
-                    }} className="cursor-pointer">Add</button>
-                </div>
-            </div>
-            <div className="absolute top-[2rem]">
-                {pop? <AddList cross={setPop}/>:null}
-           
-            </div>
-            <div className="h-[25rem] bg-white mt-[2.5rem] ml-[1.5rem] mr-[1.5rem] rounded-2xl">
-            <div className="bg-gray-100 ml-[1.5rem] grid grid-cols-2">
-            {data.map((item,i)=>{
-                return(
-                    <div className="">
-                    <h1 className="text-[2rem] font-bold ml-[5rem]">{item.title}</h1>
-                    <p>{item.description}</p>
-                    </div>
-                )
-            })}
-                
-            </div>
-            </div>
-            
+  return (
+    <Mycontext.Provider value={{ data, setdata, setPop }}>
+      <div className="bg-gray-200 h-screen">
+        <h1>.</h1>
+        <div>
+          <h1 className="bg-white m-[1rem] rounded-[20px] h-[5rem] text-[3rem] font-bold pl-[33rem]">
+            Todo List
+          </h1>
         </div>
-       
-    )
-}
+        <div className="flex justify-between">
+          <div className="bg-white flex justify-between w-[30rem] ml-[1.5rem] border-[1px]">
+            <input type="text" className="w-[30rem]" />
+            <img
+              src="https://img.icons8.com/?size=100&id=59878&format=png&color=000000"
+              alt=""
+              className="h-[2rem] bg-orange-300 p-1 cursor-pointer"
+            />
+          </div>
+          <div className="bg-green-600 mr-[10rem] w-[12rem] pl-[5rem] pt-[0.3rem] font-bold text-white cursor-pointer hover:bg-green-800">
+            <button
+              onClick={() => {
+                setPop(true);
+              }}
+              className="cursor-pointer"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="absolute top-[2rem]">{pop ? <AddList /> : null}</div>
+        <div className="h-[25rem] bg-white mt-[2.5rem] ml-[1.5rem] mr-[1.5rem] rounded-2xl">
+          <div className=" ml-[1.5rem] grid grid-cols-3">
+            {data.map((item, i) => {
+              return (
+                <div className="bg-gray-100 mt-[2rem] h-auto w-[25rem]">
+                  <h1 className="text-[2rem] font-bold ml-[10rem]">
+                    {item.title}
+                  </h1>
+                  <p className="ml-[1.5rem]">{item.description}</p>
+                  <button className="bg-blue-400 h-[2rem] w-[6rem] font-bold text-white cursor-pointer mt-[7rem]">
+                    Details
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Mycontext.Provider>
+  );
+};
 
-export default TODoList
-
+export default TODoList;
