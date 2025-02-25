d99967bd1dcff493114673003b05d6f62479d76e
Niraj7272 <nc72723@gmail.com>
2025-02-25 13:51:54 +0545

    solved database connection

--- Backend_todo/Controller/item.js
+++ Backend_todo/Controller/item.js
@@ -1,9 +1,9 @@
 import db from '../Database/db.js'
 
 export const addItem = (req,res) =>{
-    const {item,description}= req.body;
-    const q = `insert into items(item,description) values(?,?,?)`;
-    db.query(q,[item,description],(err,result)=>{
+    const {title,description}= req.body;
+    const q = `insert into items(title,description) values(?,?)`;
+    db.query(q,[title,description],(err,result)=>{
         if(err) return res.send(err);
         return res.send({result,message:"user inserted successfully"});
     });
