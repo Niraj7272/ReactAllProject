1c7e23a3e0e40f02e6867aed9d0b0a82a2d4a6d0
Niraj7272 <nc72723@gmail.com>
2025-02-25 10:59:15 +0545

    database setup

--- Todo_list/src/AddList.jsx
+++ Todo_list/src/AddList.jsx
@@ -1,8 +1,32 @@
 import React, { useContext, useState } from "react";
 import { Mycontext } from "./ToDoList";
+import axios from "axios";
 
 const AddList = () => {
   const { setPop } = useContext(Mycontext);
+  const [data, setData] = useState({
+      title:"",
+      description:"",
+  });
+
+  const handleChange = (e)=>{
+    setData({...data,[e.target.name]:e.target.value});
+  }
+
+  const handleClick = (e) =>{
+    e.preventDefault();
+    try {
+      let result = axios({
+        url:"http://localhost:4444/add-items",
+        method:"post",
+      });
+      console.log(result);
+      alert("Data Inserted successfull!");
+    } catch (error) {
+      console.log(error);
+      alert("Data Inserted Fail!");
+    }
+  }
 
   return (
     <div className="bg-blue-100 h-[25rem] w-[27rem] ml-[30rem] mt-[8rem]">
@@ -22,21 +46,26 @@ const AddList = () => {
       <div>
         <input
           type="text"
+          name="title"
+          value={data.title}
+          onChange={handleChange}
           placeholder="Title"
           className="border-[1px] m-[1rem] w-[23rem] h-[2rem] pl-[10rem] ml-[2rem]"
         />
         <br />
         <textarea
-          name=""
+          name="description"
           id=""
           cols="48"
           rows="5"
+          value={data.description}
+          onChange={handleChange}
           placeholder="Description"
           className="border-[1px] ml-[2rem]"
         ></textarea>
       </div>
       <div>
-        <button className="bg-green-400 ml-[2rem] w-[23rem] mt-[2rem] text-[1.5rem] cursor-pointer">
+        <button className="bg-green-400 ml-[2rem] w-[23rem] mt-[2rem] text-[1.5rem] cursor-pointer" onClick={handleClick}>
           Submit
         </button>
       </div>
