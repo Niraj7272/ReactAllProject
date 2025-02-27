bd76e5c19a96c46a374e39dfc7049bc41ecd076b
Niraj7272 <nc72723@gmail.com>
2025-02-27 15:19:43 +0545

    edit query

--- Backend_todo/Controller/item.js
+++ Backend_todo/Controller/item.js
@@ -1,29 +1,36 @@
-import db from '../Database/db.js'
+import db from "../Database/db.js";
 
-export const addItem = (req,res) =>{
-    const {title,description}= req.body;
-    const q = `insert into items(title,description) values(?,?)`;
-    db.query(q,[title,description],(err,result)=>{
-        if(err) return res.send(err);
-        return res.send({result,message:"user inserted successfully"});
-    });
+export const addItem = (req, res) => {
+  const { title, description } = req.body;
+  const q = `insert into items(title,description) values(?,?)`;
+  db.query(q, [title, description], (err, result) => {
+    if (err) return res.send(err);
+    return res.send({ result, message: "user inserted successfully" });
+  });
 };
 
-export const readItem = (req,res) =>{
-    const q = `select * from items`;
-    db.query(q,(err,result)=>{
-        if(err) return res.send(err);
-        return res.send(result)
-    })
-}
+export const readItem = (req, res) => {
+  const q = `select * from items`;
+  db.query(q, (err, result) => {
+    if (err) return res.send(err);
+    return res.send(result);
+  });
+};
 
-export const readSpecificItem = (req,res)=>{
+export const readSpecificItem = (req, res) => {
+  const id = req.params.id;
+  const q = `select * from items where id=?`;
+  db.query(q, [id], (err, result) => {
+    if (err) return res.send(err);
+    return res.send(result[0]);
+  });
+};
+
+export const deleteItem = (req,res) =>{
     const id = req.params.id;
-    const q = `select * from items where id=?`;
+    const q = `Delete from items WHERE id=?`;
     db.query(q,[id],(err,result)=>{
-        if(err) return res.send(err);
-        return res.send(result)
+        if(err) return res.send(err)
+            return res.send({result,message:"Item deleted successfully"}); 
     })
 }
-
-
--- Backend_todo/router/userRoute.js
+++ Backend_todo/router/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addItem, readItem, readSpecificItem } from '../Controller/item.js';
+import { addItem, deleteItem, readItem, readSpecificItem } from '../Controller/item.js';
 
 const route = express.Router();
 
 route.post("/add-item",addItem);
 route.get("/read-item",readItem);
 route.get("/readSpecific-item/:id",readSpecificItem)
+route.delete("/delete-item/:id", deleteItem)
 
 export default route;
