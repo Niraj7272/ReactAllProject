08a5e0fd507d29d876dd55ba99a369d69a2848bb
Niraj7272 <nc72723@gmail.com>
2025-02-28 15:25:36 +0545

    product page

---
+++ Test/test01/src/Frontpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Frontpage = () => {
+  return (
+    <div>
+        <div>
+            <h1 className='text-white font-bold bg-blue-300 pl-[3rem]'>ALL PRODUCTS</h1>
+        </div>
+        <div className='bg-gray-200 h-screen mt-[1rem]'>
+            <img src="./public/50-ultra-sensual-eau-de-parfum-wild-stone-men-original-imafrbzu8xgyfhkg.webp" alt="" className='h-[15rem]'/>
+        </div>
+    </div>
+  )
+}
+
+export default Frontpage
