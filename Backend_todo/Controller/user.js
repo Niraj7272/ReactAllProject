3000d2d34036d1f1fe79fef213242a745e652371
Niraj7272 <nc72723@gmail.com>
2025-03-11 19:04:38 +0545

    login validation

--- Backend_todo/Controller/user.js
+++ Backend_todo/Controller/user.js
@@ -1,5 +1,6 @@
 import db from "../Database/db.js";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 export const insertUser = (req, res) => {
   const { name, email, password } = req.body;
@@ -17,3 +18,35 @@ export const insertUser = (req, res) => {
     return res.send({ result, message: "user inserted successfully" });
   });
 };
+
+
+
+export const login = (req,res) =>{
+  const {email,password} = req.body;
+
+  const q =`Select * from users where email = ?`;
+  db.query(q,[email],(err,data)=>{
+    if (err) return res.send(err);
+    if (data.length === 0) return res.send("User not found");
+
+    console.log(data, ":Data");
+
+    const hashPassword = data[0].password;
+    const isPasswordValid = bcrypt.compareSync(password,hashPassword);
+
+    if(!isPasswordValid)
+      return res.send({message:"password is not correct"});
+
+    const token = jwt.sign({uid: data[0].id, role: data[0].role},
+      "secretkey"
+    );
+
+    console.log(token,":Token");
+
+    return res.send({
+      message:"user login successfully",
+      data: data[0],
+      token: token,
+    });
+  })
+}
--- Backend_todo/router/addUserRoute.js
+++ Backend_todo/router/addUserRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { insertUser } from '../Controller/user.js'
+import { insertUser, login } from '../Controller/user.js'
 
 const route = express.Router()
 
 route.post("/insert-user", insertUser)
+route.post("/login", login)
 
 export default route
