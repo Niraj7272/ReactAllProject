420e04170e05d73825b424efdb194814f8ec520e
Niraj7272 <nc72723@gmail.com>
2025-05-28 15:15:16 +0545

    fix the problem from product.js file

--- practice01/Backend/Controller/product.js
+++ practice01/Backend/Controller/product.js
@@ -21,10 +21,10 @@ export const addProduct = (req, res) => {
 };
 
 export const  readProduct = (req, res) => {
-    const q = `SELECT * form product`;
+    const q = `SELECT * FROM product`;
     db.query(q,(err, result) => {
-        if (err) return res.send(err)
-        return res.send(result);
+        if (err) return res.status(500).json({error: err.message})
+        return res.status(200).json({result})
     });
 }
 
@@ -32,8 +32,8 @@ export const readSpecificproduct = (req, res) => {
     const id = req.params.id;
     const q = `SELECT * FROM product WHERE idproduct=?`;
     db.query(q, [id],(err,result) => {
-        if (err) return res.send(err);
-        return res.send(result[0]);
+        if (err) return res.status(500).json({error: err.message})
+        return res.status(200).json(result[0])
     });
 }
 
@@ -41,8 +41,8 @@ export const deleteProduct = (req,res) => {
     const id = req.params.id;
     const q = `DELETE FROM product WHERE idproduct=?`;
     db.query(q,[id],(err,result)=>{
-        if (err) return res.send(err);
-        return res.send({ result, message: "Product Deleted Successfully !" });
+        if (err) return res.status(500).json({error: err.message})
+        return res.status(200).json({ result, message: "Product Deleted Successfully !" });
     });
 }
 
@@ -50,9 +50,9 @@ export const updateProduct = (req,res) => {
     const id = req.params.id;
     const { product_title, price, stock, category, description, image} = req.body;
     const q = `UPDATE product SET product_title=?, price=?, stock=?, category=?, description=?, image=? WHERE idproduct=?`;
-    db.query(q,[product_title,price,stock,quantity,description,image],(err,result) => {
-        if (err) return res.send(err);
-        return res.send({result, message:"product updated successfully!"});
+    db.query(q,[product_title,price,stock,category,description,image],(err,result) => {
+        if (err) return res.status(500).json({error: err.message})
+        return res.status(200).json({result, message:"product updated successfully!"});
     });
     // const [result]=db.query(q,[product_title,price,stock,quantity,description,image]);
     // res.status(200).json({message:"Product Updated Succesfully!",result})
