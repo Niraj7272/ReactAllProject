a6dcb82eb870f9109eb470a687eac61c7a8caef4
Niraj7272 <nc72723@gmail.com>
2025-06-02 17:38:15 +0545

    delete button added in product and custoner page

--- practice01/src/Admin/Customer.jsx
+++ practice01/src/Admin/Customer.jsx
@@ -1,99 +1,145 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { data, NavLink } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { data, NavLink } from "react-router-dom";
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 
-const Dashboard = () => {
+const Customer = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  // read User
+  const GetUser = async () => {
+    try {
+      let result = await axios({
+        url: "http://localhost:2222/read_user",
+        method: "get",
+      });
+      console.log(result.data);
+      setData(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    // read User
-    const GetUser = async() => {
-        try {
-            let result = await axios({
-                url:"http://localhost:2222/read_user",
-                method:"get",
-            });
-            console.log(result.data);
-            setData(result.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(()=>{
-        GetUser();
-    },[])
+  useEffect(() => {
+    GetUser();
+  }, []);
 
   return (
-    <div className='bg-white h-screen w-screen flex'>
-        <div className='bg-blue-900 opacity-70 h-screen w-[10rem] '>
-            <div className='flex p-[2.5rem]'>
-                <h1 className='text-orange-500 font-bold text-[1.5rem]'>E</h1>
-                <h1 className='text-white font-bold text-[1.5rem]'>Mart</h1>
-            </div>
-            <div className='text-white font-semibold ml-[2rem]'>
-                <h1 className='font-bold text-[1.1rem]'>Menu</h1>
-                <div className='mt-[1.3rem]'>
-                    <h1><NavLink to="/dashboard">Dashboard</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/product" className=''>Product</NavLink></h1>
-                    <h1 className='mt-[1.5rem] bg-gray-600 p-[0.5rem]'><NavLink to="/costomer">Customer</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/order">Order</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/category">Category</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/message">Message</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/transaction">Transaction</NavLink></h1>
-                </div>
-            </div>
+    <div className="bg-white h-screen w-screen flex">
+      <div className="bg-blue-900 opacity-70 h-screen w-[10rem] ">
+        <div className="flex p-[2.5rem]">
+          <h1 className="text-orange-500 font-bold text-[1.5rem]">E</h1>
+          <h1 className="text-white font-bold text-[1.5rem]">Mart</h1>
         </div>
+        <div className="text-white font-semibold ml-[2rem]">
+          <h1 className="font-bold text-[1.1rem]">Menu</h1>
+          <div className="mt-[1.3rem]">
+            <h1>
+              <NavLink to="/dashboard">Dashboard</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/product" className="">
+                Product
+              </NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem] bg-gray-600 p-[0.5rem]">
+              <NavLink to="/costomer">Customer</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/order">Order</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/category">Category</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/message">Message</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/transaction">Transaction</NavLink>
+            </h1>
+          </div>
+        </div>
+      </div>
 
-        <div className=''>
-        <div className='flex'>
-            <div>
-                <input type="text" placeholder='Search' className=' bg-gray-200 mt-[3rem] h-[3rem] w-[45rem] rounded-2xl ml-[2rem] pl-[2rem] font-semibold' />
-            </div>
-            <div>
-                <img src="" alt="" className=' bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]' />
-            </div>
-            <div>
-                <img src="" alt="" className=' bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]' />
-            </div>
-            <div>
-                <NavLink>
-                    <h1 className=' bg-gray-200 mt-[3rem] h-[3rem] w-[10rem] rounded-2xl ml-[2rem] pl-[3rem] pt-[0.7rem] font-bold text-gray-600'>Log Out</h1>
-                </NavLink>
-            </div>
+      <div className="">
+        <div className="flex">
+          <div>
+            <input
+              type="text"
+              placeholder="Search"
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[45rem] rounded-2xl ml-[2rem] pl-[2rem] font-semibold"
+            />
+          </div>
+          <div>
+            <img
+              src=""
+              alt=""
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]"
+            />
+          </div>
+          <div>
+            <img
+              src=""
+              alt=""
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]"
+            />
+          </div>
+          <div>
+            <NavLink>
+              <h1 className=" bg-gray-200 mt-[3rem] h-[3rem] w-[10rem] rounded-2xl ml-[2rem] pl-[3rem] pt-[0.7rem] font-bold text-gray-600">
+                Log Out
+              </h1>
+            </NavLink>
+          </div>
         </div>
-        <div className='bg-gray-200 h-[26rem] w-[67rem] ml-[2rem] mt-[3rem] pt-[1rem]'>
-            <div className='bg-white h-[2.5rem] mt-[1rem] w-[63rem] ml-[2rem] rounded-2xl'>
-                <h1 className='text-blue-900 opacity-70 font-bold text-[1.5rem] pl-[30rem] '>Customer List</h1>
-            </div>
-            <div className='bg-white mt-[2rem] h-auto w-[63rem] ml-[2rem] pl-[3rem] pt-[1rem]'>
-                <table>
-                    <thead className='text-gray-500'>
-                        <tr>
-                        <th>SN</th>
-                        <th className='pl-[10rem]'>Name</th>
-                        <th className='pl-[10rem]'>Email</th>
-                        <th className='pl-[10rem]'>Action</th>
-                        </tr>
-                    </thead>
-                    {data.map((item,i)=>{
-                        return(
-                    <tbody>
-                        <tr>
-                        <td className='pt-[1.5rem] pl-[0.5rem]'>{i+1}</td>
-                        <td className='pt-[1.5rem] pl-[7rem]'>{item.name}</td>
-                        <td className='pt-[1.5rem] pl-[10rem]'>{item.email}</td>
-                        <td className='pt-[1.5rem] pl-[10rem]'>Delete|Update</td>
-                        </tr>
-                    </tbody>
-                    );
-                })}
-                </table>
-            </div>
+        <div className="bg-gray-200 h-[26rem] w-[67rem] ml-[2rem] mt-[3rem] pt-[1rem]">
+          <div className="bg-white h-[2.5rem] mt-[1rem] w-[63rem] ml-[2rem] rounded-2xl">
+            <h1 className="text-blue-900 opacity-70 font-bold text-[1.5rem] pl-[30rem] ">
+              Customer List
+            </h1>
+          </div>
+          <div className="bg-white mt-[2rem] h-auto w-[63rem] ml-[2rem] pl-[3rem] pt-[1rem]">
+            <table>
+              <thead className="text-gray-500">
+                <tr>
+                  <th>SN</th>
+                  <th className="pl-[10rem]">Name</th>
+                  <th className="pl-[10rem]">Email</th>
+                  <th className="pl-[10rem]">Action</th>
+                </tr>
+              </thead>
+              {data.map((item, i) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td className="pt-[1.5rem] pl-[0.5rem]">{i + 1}</td>
+                      <td className="pt-[1.5rem] pl-[7rem]">{item.name}</td>
+                      <td className="pt-[1.5rem] pl-[10rem]">{item.email}</td>
+                      <td className="pt-[1.5rem] pl-[10rem]">
+                        <button
+                          className="cursor-pointer"
+                          onClick={() => {
+                    
+                          }}
+                        >
+                          <MdDelete className="text-red-700 h-[1.6rem] w-[2rem] cursor-pointer" />
+                        </button>
+
+                        <button>
+                          <CiEdit className="text-green-600 h-[1.6rem] w-[2rem] cursor-pointer" />
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Customer;
--- practice01/src/Admin/product.jsx
+++ practice01/src/Admin/product.jsx
@@ -1,139 +1,193 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
-import AddProduct from './AddProduct';
-import { MyContext } from '../Context/contextCreateandProvide';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash} from '@fortawesome/free-solid-svg-icons';
-import { toast } from 'react-toastify';
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import AddProduct from "./AddProduct";
+import { MyContext } from "../Context/contextCreateandProvide";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 
-const Product= () => {
+const Product = () => {
+  const { pop, setPop } = useContext(MyContext);
+  const [data, setData] = useState([]);
 
-    const {pop, setPop} = useContext(MyContext);
-    let params = useParams();
-    let id = params.id;
-    const [data, setData] = useState([]);
-
-    //read product
-    const GetProduct = async() => {
-        try {
-            let result = await axios({
-                url: `http://localhost:2222/read_product`,
-                method: "get",
-            });
-            console.log(result.data);
-            setData(result.data);
-        } catch (error) {
-            console.log(error);
-        }
+  //read product
+  const GetProduct = async () => {
+    try {
+      let result = await axios({
+        url: `http://localhost:2222/read_product`,
+        method: "get",
+      });
+      console.log(result.data);
+      setData(result.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    //Delete Product
-    const DeleteProduct = async(id)=>{
-        try {
-            let result = await axios({
-                url:`http://localhost:2222/delete_product/${id}`,
-                method: "delete",
-            })
-            GetProduct();
-            toast.success(result.data.message);
-        } catch (error) {
-            console.log(error);
-        }
+  //Delete Product
+  const DeleteProduct = async (id) => {
+    try {
+      let result = await axios({
+        url: `http://localhost:2222/delete_product/${id}`,
+        method: "delete",
+      });
+      GetProduct();
+      toast.success(result.data.message);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(()=>{
-        GetProduct();
-        console.log(data);
-    },[]);
+  useEffect(() => {
+    GetProduct();
+    console.log(data);
+  }, []);
 
   return (
-    <div className='bg-white h-screen w-screen flex'>
-        <div className='bg-blue-900 opacity-70 h-screen w-[10rem] '>
-            <div className='flex p-[2.5rem]'>
-                <h1 className='text-orange-500 font-bold text-[1.5rem]'>E</h1>
-                <h1 className='text-white font-bold text-[1.5rem]'>Mart</h1>
-            </div>
-            <div className='text-white font-semibold ml-[2rem]'>
-                <h1 className='font-bold text-[1.1rem]'>Menu</h1>
-                <div className='mt-[1.3rem]'>
-                    <h1><NavLink to="/dashboard">Dashboard</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/product" className='bg-gray-600 p-[0.5rem]'>Product</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/costomer">Customer</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/order">Order</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/category">Category</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/message">Message</NavLink></h1>
-                    <h1 className='mt-[1.5rem]'><NavLink to="/transaction">Transaction</NavLink></h1>
-                </div>
-            </div>
+    <div className="bg-white h-screen w-screen flex">
+      <div className="bg-blue-900 opacity-70 h-screen w-[10rem] ">
+        <div className="flex p-[2.5rem]">
+          <h1 className="text-orange-500 font-bold text-[1.5rem]">E</h1>
+          <h1 className="text-white font-bold text-[1.5rem]">Mart</h1>
+        </div>
+        <div className="text-white font-semibold ml-[2rem]">
+          <h1 className="font-bold text-[1.1rem]">Menu</h1>
+          <div className="mt-[1.3rem]">
+            <h1>
+              <NavLink to="/dashboard">Dashboard</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/product" className="bg-gray-600 p-[0.5rem]">
+                Product
+              </NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/costomer">Customer</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/order">Order</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/category">Category</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/message">Message</NavLink>
+            </h1>
+            <h1 className="mt-[1.5rem]">
+              <NavLink to="/transaction">Transaction</NavLink>
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex relative">
+          <div className="absolute">{pop ? <AddProduct /> : null}</div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search"
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[45rem] rounded-2xl ml-[2rem] pl-[2rem] font-semibold"
+            />
+          </div>
+          <div>
+            <img
+              src=""
+              alt=""
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]"
+            />
+          </div>
+          <div>
+            <img
+              src=""
+              alt=""
+              className=" bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]"
+            />
+          </div>
+          <div>
+            <NavLink>
+              <h1 className=" bg-gray-200 mt-[3rem] h-[3rem] w-[10rem] rounded-2xl ml-[2rem] pl-[3rem] pt-[0.7rem] font-bold text-gray-600">
+                Log Out
+              </h1>
+            </NavLink>
+          </div>
         </div>
 
-        <div>
-        <div className='flex relative'>
-        <div className='absolute'>{pop ? <AddProduct/> : null}</div>
-            <div>
-                <input type="text" placeholder='Search' className=' bg-gray-200 mt-[3rem] h-[3rem] w-[45rem] rounded-2xl ml-[2rem] pl-[2rem] font-semibold' />
-            </div>
-            <div>
-                <img src="" alt="" className=' bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]' />
-            </div>
-            <div>
-                <img src="" alt="" className=' bg-gray-200 mt-[3rem] h-[3rem] w-[3rem] rounded-2xl ml-[2rem] pl-[2rem]' />
-            </div>
-            <div>
-                <NavLink>
-                    <h1 className=' bg-gray-200 mt-[3rem] h-[3rem] w-[10rem] rounded-2xl ml-[2rem] pl-[3rem] pt-[0.7rem] font-bold text-gray-600'>Log Out</h1>
-                </NavLink>
-            </div>
+        <div className="bg-gray-200 h-[30rem] w-[70rem] mt-[2rem] ml-[2rem]">
+          <div className="flex">
+            <h1 className="font-bold text-[1.5rem] text-blue-800 pl-[25rem] mt-[1rem] bg-white h-[2.5rem] w-[55rem] rounded-2xl ml-[2rem]">
+              Product List
+            </h1>
+            <button
+              onClick={() => {
+                setPop(true);
+              }}
+              className="bg-green-600 h-[2rem] mt-[1rem] w-[7.5rem] pl-[1rem] font-bold text-white cursor-pointer ml-[4rem]"
+            >
+              Add
+            </button>
+          </div>
+          <div className="bg-white h-auto w-[66rem] m-[2rem] pl-[3rem] pt-[1rem]">
+            <table>
+              <thead className="text-gray-500">
+                <tr>
+                  <th>SN</th>
+                  <th className="">Name</th>
+                  <th className="pl-[3rem]">Price</th>
+                  <th className="pl-[3rem]">Stock</th>
+                  <th className="pl-[3rem]">Category</th>
+                  <th className="pl-[5rem]">Description</th>
+                  <th className="pl-[5rem]">Image</th>
+                  <th className="pl-[5rem]">Action</th>
+                </tr>
+              </thead>
+              {data.map((item, i) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td className="pt-[1rem]">{1 + i}</td>
+                      <td className="pt-[1rem] pl-[1.5rem]">
+                        {item.product_title}
+                      </td>
+                      <td className="pt-[1rem] pl-[3rem]">{item.price}</td>
+                      <td className="pt-[1rem] pl-[3rem]">{item.stock}</td>
+                      <td className="pt-[1rem] pl-[3rem]">{item.category}</td>
+                      <td className="pt-[1rem] pl-[2rem]">
+                        {item.description}
+                      </td>
+                      <td className="pt-[1rem] pl-[5rem]">
+                        <img
+                          src={`http://localhost:2222/images/${item.image}`}
+                          alt=""
+                          height="50px"
+                          width="100px"
+                        />
+                      </td>
+                      <td className="pt-[1rem] pl-[3rem]">
+                        <button
+                          className="cursor-pointer"
+                          onClick={() => {
+                            DeleteProduct(item.idproduct);
+                          }}
+                        >
+                          <MdDelete className="text-red-700 h-[1.6rem] w-[2rem] cursor-pointer" />
+                        </button>
+            
+                        <button>
+                          <CiEdit className="text-green-600 h-[1.6rem] w-[2rem] cursor-pointer" />
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
         </div>
-        
-        <div className='bg-gray-200 h-[30rem] w-[70rem] mt-[2rem] ml-[2rem]'>
-            <div className='flex'>
-                <h1 className='font-bold text-[1.5rem] text-blue-800 pl-[25rem] mt-[1rem] bg-white h-[2.5rem] w-[55rem] rounded-2xl ml-[2rem]'>Product List</h1>
-                <button onClick={()=>{setPop(true)}} className='bg-green-600 h-[2rem] mt-[1rem] w-[7.5rem] pl-[1rem] font-bold text-white cursor-pointer ml-[4rem]'>Add</button>
-            </div>
-            <div className='bg-white h-auto w-[66rem] m-[2rem] pl-[3rem] pt-[1rem]'>
-                <table>
-                    <thead className='text-gray-500'>
-                        <tr>
-                            <th>SN</th>
-                            <th className=''>Name</th>
-                            <th className='pl-[3rem]'>Price</th>
-                            <th className='pl-[3rem]'>Stock</th>
-                            <th className='pl-[3rem]'>Category</th>
-                            <th className='pl-[5rem]'>Description</th>
-                            <th className='pl-[5rem]'>Image</th>
-                            <th className='pl-[5rem]'>Action</th>
-                        </tr>
-                    </thead>
-                    {data.map((item,i) =>{
-                return(
-                    <tbody>
-                        <tr>
-                            <td className='pt-[1rem]'>{1+i}</td>
-                            <td className='pt-[1rem] pl-[1.5rem]'>{item.product_title}</td>
-                            <td className='pt-[1rem] pl-[3rem]'>{item.price}</td>
-                            <td className='pt-[1rem] pl-[3rem]'>{item.stock}</td>
-                            <td className='pt-[1rem] pl-[3rem]'>{item.category}</td>
-                            <td className='pt-[1rem] pl-[2rem]'>{item.description}</td>
-                            <td className='pt-[1rem] pl-[5rem]'><img src={`http://localhost:2222/images/${item.image}`} alt="" height="50px" width="100px" /></td>
-                            <td className='pt-[1rem] pl-[3rem]'>
-                                <button className='cursor-pointer' onClick={()=>{
-                                    DeleteProduct(item.id);
-                                }}>
-                                    <FontAwesomeIcon icon={ faTrash} style={{color: "#ce0912",}} />
-                                </button>|
-                            </td>
-                        </tr>
-                    </tbody>
-                       )
-                    })}
-                </table>
-            </div>
-        </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
