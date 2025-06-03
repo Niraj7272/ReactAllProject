be06af2a250d3d4eb3f9586b996ec7b4d2681b16
Niraj7272 <nc72723@gmail.com>
2025-06-04 01:02:41 +0545

    giving proper look to home

--- practice01/src/Pages/Home.jsx
+++ practice01/src/Pages/Home.jsx
@@ -2,52 +2,110 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-[2000px] relative">
-      <div className="sticky top-0 z-10">
-        <img
-          src="src/assets/red-wine-bottle-wine-glass-burgundy-background_884546-2883.jpg"
-          alt=""
-          className="h-[32.8rem] ml-[1.5rem] bg-white"
-        />
-      </div>
-      <div className="relative z-20 mt-[-25rem] px-6">
-        <div className="bg-transparent h-[30rem] w-screen inline-block ml-[4rem]">
-          <h4 className="text-white text-[1rem]">Uncork the New Elegance</h4>
-          <h1 className="text-white text-[3rem] w-[40rem] font-bold">
-            Introducing our latest wine collection
-          </h1>
-          <h3 className="text-white text-[1.2rem] w-[40rem]">
-            Experience bold flavors and timeless craftsmanship with every pour
-            of our newest vintage selection
-          </h3>
-          <button className="bg-white mt-[3rem] h-[2.5rem] w-[9rem] cursor-pointer hover:text-blue-400">
-            SHOP NOW
-          </button>
+    <div>
+      <div className="h-[1130px] relative">
+        <div className="sticky top-0 z-10">
+          <img
+            src="src/assets/red-wine-bottle-wine-glass-burgundy-background_884546-2883.jpg"
+            alt=""
+            className="h-[32.8rem] ml-[1.5rem] bg-white"
+          />
         </div>
-        <div className="bg-white h-[60rem] w-screen inline-block p-4">
-          <div className="flex flex-col items-center justify-center p-4 text-center">
-            <h1 className="text-gray-500">POPULAR PRODUCTS</h1>
-            <h1 className="text-gray-800 font-bold text-[2.5rem]">Trending Now</h1>
+        <div className="relative z-20 mt-[-25rem] px-6">
+          <div className="bg-transparent h-[30rem] w-screen inline-block ml-[4rem]">
+            <h4 className="text-white text-[1rem]">Uncork the New Elegance</h4>
+            <h1 className="text-white text-[3rem] w-[40rem] font-bold">
+              Introducing our latest wine collection
+            </h1>
+            <h3 className="text-white text-[1.2rem] w-[40rem]">
+              Experience bold flavors and timeless craftsmanship with every pour
+              of our newest vintage selection
+            </h3>
+            <button className="bg-white mt-[3rem] h-[2.5rem] w-[9rem] cursor-pointer hover:text-blue-400">
+              SHOP NOW
+            </button>
           </div>
-          <div className="flex gap-[2rem]">
-            <img src="src/assets/nikeairforce.jpeg" alt="" />
-            <img src="src/assets/mackbook.jpeg" alt="" />
-            <img src="src/assets/merlotveneto.jpeg" alt="" />
-            <img src="src/assets/images (4).jpeg" alt="" />
-            <img src="src/assets/iphone12.jpeg" alt="" />
+          <div className="bg-white h-[60rem] w-screen inline-block p-4">
+            <div className="flex flex-col items-center justify-center p-4 text-center">
+              <h1 className="text-gray-500">POPULAR PRODUCTS</h1>
+              <h1 className="text-gray-800 font-bold text-[2.5rem]">
+                Trending Now
+              </h1>
+            </div>
+            <div className="flex gap-[2rem]">
+              <img src="src/assets/nikeairforce.jpeg" alt="" />
+              <img src="src/assets/mackbook.jpeg" alt="" />
+              <img src="src/assets/merlotveneto.jpeg" alt="" />
+              <img src="src/assets/images (4).jpeg" alt="" />
+              <img src="src/assets/iphone12.jpeg" alt="" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center mt-[7rem]">
+              <h1 className="text-gray-500">SHOP</h1>
+              <h1 className="text-gray-800 font-bold text-[2.5rem]">
+                Best Selling
+              </h1>
+            </div>
+            <div className="flex gap-[2rem]">
+              <img src="src/assets/iphone12.jpeg" alt="" />
+              <img src="src/assets/images (4).jpeg" alt="" />
+              <img src="src/assets/mackbook.jpeg" alt="" />
+              <img src="src/assets/nikeairforce.jpeg" alt="" />
+              <img src="src/assets/merlotveneto.jpeg" alt="" />
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center text-center mt-[7rem]">
-            <h1 className="text-gray-500">SHOP</h1>
-            <h1 className="text-gray-800 font-bold text-[2.5rem]">Best Selling</h1>
-          </div>
-          <div className="flex gap-[2rem]">
-            <img src="src/assets/iphone12.jpeg" alt="" />
-            <img src="src/assets/images (4).jpeg" alt="" />
-            <img src="src/assets/mackbook.jpeg" alt="" />
-            <img src="src/assets/nikeairforce.jpeg" alt="" />
-            <img src="src/assets/merlotveneto.jpeg" alt="" />
-          </div>
+        </div>
+      </div>
 
+      <div className="h-[9000px] relative">
+        <div className="sticky top-0 z-10">
+          <img
+            src="src/assets/حديث_صحيح_عن_تحريم_الخمر.jpg"
+            alt=""
+            className="h-[39rem] ml-[1.5rem] bg-white"
+          />
+        </div>
+        <div className="relative z-20 mt-[-4rem] px-6">
+          <div className="bg-transparent h-[30rem] w-screen inline-block ml-[4rem]">
+            <h3 className="text-white text-[1rem] w-[30rem]">NEW COLLECTION</h3>
+            <h1 className="text-white font-bold text-[3rem] w-[30rem]">
+              Raise a Glass to Something Special
+            </h1>
+            <h1 className="text-white text-[1.5rem] w-[30rem]">
+              Discover the essence of our newest wine release Savor rich aromas,
+              refined taste, and the artistry of tradition in every bottle.
+            </h1>
+            <button className="bg-white mt-[3rem] h-[2.5rem] w-[9rem] cursor-pointer hover:text-blue-400">
+              SHOP NOW
+            </button>
+          </div>
+          <div className="bg-white h-[60rem] w-screen inline-block p-4">
+            <div className="flex flex-col items-center justify-center p-4 text-center">
+              <h1 className="text-gray-500">POPULAR PRODUCTS</h1>
+              <h1 className="text-gray-800 font-bold text-[2.5rem]">
+                Trending Now
+              </h1>
+            </div>
+            <div className="flex gap-[2rem]">
+              <img src="src/assets/nikeairforce.jpeg" alt="" />
+              <img src="src/assets/mackbook.jpeg" alt="" />
+              <img src="src/assets/merlotveneto.jpeg" alt="" />
+              <img src="src/assets/images (4).jpeg" alt="" />
+              <img src="src/assets/iphone12.jpeg" alt="" />
+            </div>
+            <div className="flex flex-col items-center justify-center text-center mt-[7rem]">
+              <h1 className="text-gray-500">SHOP</h1>
+              <h1 className="text-gray-800 font-bold text-[2.5rem]">
+                Best Selling
+              </h1>
+            </div>
+            <div className="flex gap-[2rem]">
+              <img src="src/assets/iphone12.jpeg" alt="" />
+              <img src="src/assets/images (4).jpeg" alt="" />
+              <img src="src/assets/mackbook.jpeg" alt="" />
+              <img src="src/assets/nikeairforce.jpeg" alt="" />
+              <img src="src/assets/merlotveneto.jpeg" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
