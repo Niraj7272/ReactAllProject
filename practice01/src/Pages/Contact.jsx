f4ad8bc110201b24bed9488eb40398959d8d6670
Niraj7272 <nc72723@gmail.com>
2025-06-07 23:11:04 +0545

    contact page

--- practice01/src/Pages/Contact.jsx
+++ practice01/src/Pages/Contact.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <div>
-      <div>
-          <img src="src/assets/contact.jpg" alt="" className='w-screen h-[60vh] relative opacity-80' />
-          <div className='flex justify-center'>
-              <h1 className='absolute top-[13rem] text-white font-bold text-[3.5rem]'>Contact Us</h1>
-          </div>
-      </div>
-      <div>
-        <h1 className='ml-[29rem] text-[2rem] font-bold text-gray-500'>Let's Start a Conversation</h1>
-        <div className='flex justify-between'>
-          <div className='ml-[5rem]'>
-            <h1 className='font-bold text-[2rem] '>How TO Find Us</h1>
-            <p className=''>hello fsdfufh duhdc </p>
-          </div>
-          <div className='mr-[10rem]'>
-            <h1 className='font-bold text-[2rem]'>Get IN Touch</h1>
-          </div>
+      <div className="bg-red-300 h-screen w-[83rem]">
+        <div className="bg-gray-200 h-[20rem] w-[30rem] ml-[10rem]">
+          <h1 className="text-gray-800 font-bold text-[2rem]">Contact Me</h1>
+          <p>
+            I Will read all my emails one by one and i'm not going to let them
+            missed.Send me any message you want and i will reply that.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
