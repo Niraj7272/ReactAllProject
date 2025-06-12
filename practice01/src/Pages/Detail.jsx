33633ffa0a8f02277980405086437b7ca41d91aa
Niraj7272 <nc72723@gmail.com>
2025-06-12 22:33:09 +0545

    djvkf

--- practice01/src/Pages/Detail.jsx
+++ practice01/src/Pages/Detail.jsx
@@ -3,51 +3,47 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
-    const [data, setData] = useState([]);
-    const {idproduct} = useParams();
-    console.log(idproduct);
+  const [data, setData] = useState([]);
+  const { idproduct } = useParams();
 
-    const GetDetail = async() => {
-        try {
-            let result = await axios({
-                url: `http://localhost:2222/readSpecific_product/${idproduct}`,
-                method: "get",
-            })
-            console.log(result.data);
-            setData(result.data);
-        } catch (error) {
-            console.log(error);
-        }
+  // console.log(idproduct);
 
-        useEffect(()=>{
-            GetDetail();
-        },[])
+  const GetDetail = async () => {
+    try {
+      let result = await axios({
+        url: `http://localhost:2222/readSpecific_product/${idproduct}`,
+        method: "get",
+      });
+      console.log(result.data);
+      setData(result.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  useEffect(() => {
+    GetDetail();
+  }, []);
 
   return (
     <div>
       <div className="bg-white h-screen w-screen flex">
-        {data.map((item,i)=>{
-            return(
-        <div>
-          <img
-            src={`http://localhost:2222/images/${item.image}`}
-            alt=""
-            className="h-[30rem] pt-[3rem] pl-[5rem] p-[2rem] "
-          />
-          {/* <div className="flex h-[6rem] ml-[5rem] gap-[0.5rem]">
+            <div>
+              <img
+                src={`http://localhost:2222/images/${data.image}`}
+                alt=""
+                className="h-[30rem] pt-[3rem] pl-[5rem] p-[2rem] "
+              />
+              {/* <div className="flex h-[6rem] ml-[5rem] gap-[0.5rem]">
             <img src="src/assets/nikeairforce.jpeg" alt="" />
             <img src="src/assets/nikeairforce.jpeg" alt="" />
             <img src="src/assets/nikeairforce.jpeg" alt="" />
             <img src="src/assets/nikeairforce.jpeg" alt="" />
           </div> */}
-        </div>
-        )
-  })}
+            </div>
         <div className="mt-[4rem]">
-          <h1 className="font-bold text-2xl text-gray-600">Nike Air Force</h1>
+          <h1 className="font-bold text-2xl text-gray-600">{data.product_title}</h1>
           <h2 className="font-bold text-2xl mt-[1rem] text-gray-500">
-            Rs. 100000
+            Rs. {data.price}
           </h2>
           <select
             name=""
@@ -68,10 +64,7 @@ const Detail = () => {
             Product Details
           </h1>
           <p className="w-[30rem] mt-[1rem] text-gray-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque,
-            repudiandae? Laborum, sunt nihil consectetur earum quod sequi rem
-            consequatur dicta similique tempore libero esse fuga veritatis
-            doloremque nemo! Dolor, soluta?
+            {data.description}
           </p>
         </div>
       </div>
@@ -81,11 +74,15 @@ const Detail = () => {
         </h1>
         <hr className="text-orange-600 w-[5rem] ml-[43rem] mt-[0.8rem]" />
         <div className="flex gap-[1.5rem] mt-[2rem]">
-            <img src="src/assets/mackbook.jpeg" alt="" />
-            <img src="src/assets/iphone12.jpeg" alt="" />
-            <img src="src/assets/50-ultra-sensual-eau-de-parfum-wild-stone-men-original-imafrbzu8xgyfhkg.webp" alt="" className="h-[12rem]"/>
-            <img src="src/assets/tshirtblack.jpg" alt="" className="h-[12rem]"/>
-            <img src="src/assets/nikeairforce.jpeg" alt="" />
+          <img src="src/assets/mackbook.jpeg" alt="" />
+          <img src="src/assets/iphone12.jpeg" alt="" />
+          <img
+            src="src/assets/50-ultra-sensual-eau-de-parfum-wild-stone-men-original-imafrbzu8xgyfhkg.webp"
+            alt=""
+            className="h-[12rem]"
+          />
+          <img src="src/assets/tshirtblack.jpg" alt="" className="h-[12rem]" />
+          <img src="src/assets/nikeairforce.jpeg" alt="" />
         </div>
       </div>
     </div>
